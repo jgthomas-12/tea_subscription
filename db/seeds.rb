902cc3a6bb7a5bcf0f801044e8fbda35ae192353
Jesse Thomas <jgthomas3@gmail.com>
2023-09-19 21:17:12 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  FactoryBot.create(:customer)
+end
+
+20.times do
+  FactoryBot.create(:tea)
+end
+
+30.times do
+  FactoryBot.create(:subscription, customer: Customer.all.sample, tea: Tea.all.sample)
+end
